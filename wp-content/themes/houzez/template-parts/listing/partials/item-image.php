b6996f3a49817f6eb167cfb8241fd0f67d85088e
ajaynrao <ajaynagotha.rao@gmail.com>
2021-09-17 19:30:47 +0530

    add video thumbnails on every property listing pages

--- wp-content/themes/houzez/template-parts/listing/partials/item-image.php
+++ wp-content/themes/houzez/template-parts/listing/partials/item-image.php
@@ -5,8 +5,9 @@ $thumbnail_size = !empty($ele_thumbnail_size) ? $ele_thumbnail_size : 'houzez-it
 	<div class="listing-thumb">
 		<a href="javascript:void(0)" class="listing-featured-thumb hover-effect">
 			<?php
-		    
-		    if( has_post_thumbnail( $post->ID ) && get_the_post_thumbnail($post->ID) != '' ) {
+		    if(has_post_video( $post->ID )) {
+				the_post_video( $size );
+			}elseif( has_post_thumbnail( $post->ID ) && get_the_post_thumbnail($post->ID) != '' ) {
 		        the_post_thumbnail( $thumbnail_size, array('class' => 'img-fluid') );
 		    }else{
 		        houzez_image_placeholder( $thumbnail_size );
